2d43cbb1599020a03580dfc0073c25720353189b
samuel <samoffem@gmail.com>
2022-01-17 16:08:21 +0100

    screens added

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Onboarding } from './app/screens';
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
+
+const getFonts = ()=> Font.loadAsync({
+  'roboto-black': require('./app/assets/fonts/Roboto-Black.ttf'),
+  'roboto-black-italic': require('./app/assets/fonts/Roboto-BlackItalic.ttf'),
+  'roboto-bold': require('./app/assets/fonts/Roboto-Bold.ttf'),
+  'roboto-boldItalic': require('./app/assets/fonts/Roboto-BoldItalic.ttf'),
+  'roboto-Italic': require('./app/assets/fonts/Roboto-Italic.ttf'),
+  'roboto-regular': require('./app/assets/fonts/Roboto-Regular.ttf'),
+  'roboto-thin': require('./app/assets/fonts/Roboto-Thin.ttf')
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded, setFontsloaded] = useState(false)
+  const Stack = createStackNavigator()
+
+  if(fontsLoaded){
+    return (
+      <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name="Onboarding" component={Onboarding} options={{headerShown: false}} />
+       </Stack.Navigator>
+      </NavigationContainer>
+     );
+  }else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={()=> setFontsloaded(true)}
+        onError={()=>console.log('error')}
+      />
+    )
+  }
+  
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
---
+++ app/constants/images.js
@@ -0,0 +1,9 @@
+export const onboarding1 = require('../assets/images/onboarding1.png')
+export const onboarding2 = require('../assets/images/onboarding2.png')
+export const onboarding3 = require('../assets/images/onboarding3.png')
+
+export default {
+    onboarding1,
+    onboarding2,
+    onboarding3
+}
